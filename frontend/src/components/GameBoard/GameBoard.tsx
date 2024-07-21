7400c1a327ce447a9662d6cd1440e6637fed1cd6
oblionC <tanmaythegreat17@gmail.com>
2024-07-21 15:49:45 +0530

    change project

---
+++ frontend/src/components/GameBoard/GameBoard.tsx
@@ -0,0 +1,11 @@
+import styles from './GameBoard.module.css'
+
+export default function GameBoard() {
+    return (
+        <>
+            <div className={styles.gameBoard}>
+                This is a gameboard
+            </div>
+        </>
+    )
+}
