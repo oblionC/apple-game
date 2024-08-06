8b116da4442de2671789fc4670e303d3e0eddd28
oblionC <tanmaythegreat17@gmail.com>
2024-08-07 02:30:35 +0530

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Apple game
 Multiplayer game based on https://en.gamesaien.com/game/fruit_box/
+
+To-do list:
+[] Timer and score counter
+[] Landing page
+[] Google/Github signup using passport js
+[] User page with statistics (highscores for gamemodes, clear rate, etc)
+[] Real time multiplayer (sockets)
+[] Game modes (time trouble, zen mode)
--- frontend/src/components/pages/HomePage/HomePage.tsx
+++ frontend/src/components/pages/HomePage/HomePage.tsx
@@ -6,8 +6,8 @@ export default function HomePage() {
         <>
             <div className="w-screen h-screen flex flex-row">
                 <Sidebar /> 
-                <div className="w-full flex-grow border-black">
-                    <GameScreen width={1500} height={window.innerHeight} /> 
+                <div className="w-full flex-grow">
+                    <GameScreen width={1100} height={window.innerHeight} /> 
                 </div>
             </div>
         </>
