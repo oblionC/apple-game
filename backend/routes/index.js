a0bb0b0ee272afb2c074fd8042db9b35ac1884cf
oblionC <tanmaythegreat17@gmail.com>
2024-12-27 23:10:21 +0530

    update backend docker build

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send({status: "working"});
+  res.status(200).send({status: "working"});
 });
 
 module.exports = router;
