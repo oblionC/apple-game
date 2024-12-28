990e633d731c319e41208ff0ad6bf47f04209a5f
oblionC <tanmaythegreat17@gmail.com>
2024-12-28 23:22:10 +0530

    Update index route

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.status(200).send({status: "working"});
+  res.sendStatus(200);
 });
 
 module.exports = router;
